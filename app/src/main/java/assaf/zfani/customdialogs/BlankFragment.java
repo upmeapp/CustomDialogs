1daac1ddf41d60f5837551d1cd01622de88f9483
matan <rahavmatan@gmail.com>
2017-10-30 16:44:54 +0200

    Initial commit

---
+++ app/src/main/java/assaf/zfani/customdialogs/BlankFragment.java
@@ -0,0 +1,32 @@
+package assaf.zfani.customdialogs;
+
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends DialogFragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+
+}
---
+++ app/src/main/java/assaf/zfani/customdialogs/MainActivity.java
@@ -0,0 +1,14 @@
+package assaf.zfani.customdialogs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new BlankFragment().show(getFragmentManager(),"my");
+    }
+}
